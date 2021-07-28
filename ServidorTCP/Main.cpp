f998f72fa95bc82ddc5270946e3c11fdd807165a
LinuxPingu <47802332+LinuxPingu@users.noreply.github.com>
2021-07-28 13:59:31 -0600

    Add project files.

---
+++ ServidorTCP/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <ws2tcpip.h>
+#pragma comment (lib,"ws2_32.lib")
+using namespace std;
+
+void main() {
+
+	// 1) Initializae Winsock
+	// 2) Create a Socket
+	// 3) Bind a Socket to an Ip Adress and Port
+	// 4) Tell Winsock the socket is for listening
+	// 5) Wait for Connection
+	// 6) Close Listening  socket
+	// 7) While loop: accept and echo message back to client
+	// 8) Close Socket
+	// 9) Shutdown Winsock
+
+
+}
