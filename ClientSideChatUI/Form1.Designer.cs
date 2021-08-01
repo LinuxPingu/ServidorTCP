161dbecd6f38cbe74d991d901ca2f4477810799f
jarroyo1112 <87585442+jarroyo1112@users.noreply.github.com>
2021-08-01 15:24:00 -0600

    Interfaz grafica ClientSideChatUI1
    Código y botones para la interfaz gráfica

--- ClientSideChatUI/Form1.Designer.cs
+++ ClientSideChatUI/Form1.Designer.cs
@@ -29,13 +29,65 @@ namespace ClientSideChatUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.boton_enviar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(633, 304);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 351);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(525, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // boton_enviar
+            // 
+            this.boton_enviar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.boton_enviar.Location = new System.Drawing.Point(554, 344);
+            this.boton_enviar.Name = "boton_enviar";
+            this.boton_enviar.Size = new System.Drawing.Size(91, 34);
+            this.boton_enviar.TabIndex = 2;
+            this.boton_enviar.Text = "Enviar";
+            this.boton_enviar.UseVisualStyleBackColor = true;
+            this.boton_enviar.Click += new System.EventHandler(this.Enviar_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.boton_enviar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(657, 386);
+            this.Controls.Add(this.boton_enviar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button boton_enviar;
     }
 }
 
--- ClientSideChatUI/Form1.cs
+++ ClientSideChatUI/Form1.cs
@@ -1,23 +1,76 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ClientSideChatUI
 {
     public partial class Form1 : Form
     {
+        TcpClient _client;
+
+        byte[] _buffer = new byte[4096];
         public Form1()
         {
             InitializeComponent();
 
+            _client = new TcpClient();
+            _client.Connect(" ", 54000);
+            _client.GetStream().BeginRead(_buffer, 0,
+                                          _buffer.Length,
+                                          Server_MessageReceived,
+                                          null);
+        }
+
+        private void Server_MessageReceived(IAsyncResult ar)
+        {
+            if (ar.IsCompleted)
+            {
+                // End the stream read
+                var bytesIn = _client.GetStream().EndRead(ar);
+                if (bytesIn > 0)
+                {
+                    // Create a string from the received data. For this server 
+                    // our data is in the form of a simple string, but it could be
+                    // binary data or a JSON object. Payload is your choice.
+                    var tmp = new byte[bytesIn];
+                    Array.Copy(_buffer, 0, tmp, 0, bytesIn);
+                    var str = Encoding.ASCII.GetString(tmp);
+
+                    // Any actions that involve interacting with the UI must be done
+                    // on the main thread. This method is being called on a worker
+                    // thread so using the form's BeginInvoke() method is vital to
+                    // ensure that the action is performed on the main thread.
+                    BeginInvoke((Action)(() =>
+                    {
+                        listBox1.Items.Add(str);
+                        listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                    }));
+                }
+
+                // Clear the buffer and start listening again
+                Array.Clear(_buffer, 0, _buffer.Length);
+                _client.GetStream().BeginRead(_buffer, 0,
+                                              _buffer.Length,
+                                              Server_MessageReceived,
+                                              null);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
 
         }
 
+        private void Enviar_Click(object sender, EventArgs e)
+        {
+            // Encode the message and send it out to the server.
+            var msg = Encoding.ASCII.GetBytes(textBox1.Text);
+            _client.GetStream().Write(msg, 0, msg.Length);
+
+            // Clear the text box and set it's focus
+            textBox1.Text = "";
+            textBox1.Focus();
+        }
     }
 }
